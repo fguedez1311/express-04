caa4c41231b5f56d8284b92c9863dc655e93c617
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2026-05-07 15:05:03 -0400

    feact:Creación de la paginación

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express, {request, response } from 'express'
 import process from 'node:process'
 import jobs from './jobs.json' with {type:'json'}
+import {DEFAULTS} from './config.js'
 // Devuelve los segundos que el proceso lleva activo
 const uptime = process.uptime()
 
-const PORT=process.env.PORT ?? 1234
+const PORT=process.env.PORT ?? DEFAULTS.PORT
 
 const  app=express()
 app.use('/',(request,response,next)=>{
@@ -25,7 +26,27 @@ app.get('/health',(request,response)=>{
 })
 app.get('/get-jobs',async(request,response)=>{
     // const {default:jobs}=await import('./jobs.json',{with:{type:'json'}})
-    return response.json(jobs)
+    const {text,title,level,limit=DEFAULTS.LIMIT_PAGINATION,technology,offset=DEFAULTS.LIMIT_OFFSET}=request.query
+    let filteredJobs=jobs
+
+    if (text){
+        const searchTerm=text.toLocaleLowerCase()
+        filteredJobs=filteredJobs.filter(job=>job.titulo.toLowerCase().includes(searchTerm) || job.descripcion.toLowerCase().includes(searchTerm)
+        )
+       
+    }
+    
+
+   // Si no hay filtro, devolvemos todos los empleos
+     const limitNumber=Number(limit)
+     const offsetNumber=Number(offset)
+     const paginatedJobs=filteredJobs.slice(offsetNumber,offsetNumber+limitNumber)
+     return response.json(paginatedJobs)
+
+
+
+
+    
 })
 app.get('/get-single-job/:id',(request,response)=>{
     const {id}=request.params
