32b1b28e578c81b1483a6460b19e29962f8c577a
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2026-05-11 05:56:27 -0400

    feat:Creación de un nuevo recurso en el servidor con post

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, {request, response } from 'express'
+import express from 'express'
 import process from 'node:process'
 import jobs from './jobs.json' with {type:'json'}
 import {DEFAULTS} from './config.js'
@@ -8,25 +8,30 @@ const uptime = process.uptime()
 const PORT=process.env.PORT ?? DEFAULTS.PORT
 
 const  app=express()
-app.use('/',(request,response,next)=>{
+app.use(express.json())
+
+app.use('/',(req,res,next)=>{
     const timeString=new Date().toLocaleDateString()
-    console.log(`[${timeString} ${request.method} ${request.url}]`)
+    console.log(`[${timeString} ${req.method} ${req.url}]`)
     next()
 })
 
-app.get('/',(request,response)=>{
-    return response.send('<h1>Hello Word!</h1>')
+app.get('/',(req,res)=>{
+    return res.send('<h1>Hello Word!</h1>')
 })
-app.get('/health',(request,response)=>{
-    return response.json({
+app.get('/health',(req,res)=>{
+    return res.json({
         status:'ok',
         uptime: Math.floor(process.uptime()), // Segundos ndeados
         timestamp: Date.now(),
     })
 })
-app.get('/get-jobs',async(request,response)=>{
+
+// Crud: Create, Read, Update,Delete
+ 
+app.get('/get-jobs',async(req,res)=>{
     // const {default:jobs}=await import('./jobs.json',{with:{type:'json'}})
-    const {text,title,level,limit=DEFAULTS.LIMIT_PAGINATION,technology,offset=DEFAULTS.LIMIT_OFFSET}=request.query
+    const {text,title,level,limit=DEFAULTS.LIMIT_PAGINATION,technology,offset=DEFAULTS.LIMIT_OFFSET}=req.query
     let filteredJobs=jobs
 
     if (text){
@@ -41,27 +46,38 @@ app.get('/get-jobs',async(request,response)=>{
      const limitNumber=Number(limit)
      const offsetNumber=Number(offset)
      const paginatedJobs=filteredJobs.slice(offsetNumber,offsetNumber+limitNumber)
-     return response.json(paginatedJobs)
+     return res.json(paginatedJobs)
 
 
 
 
     
 })
-app.get('/get-single-job/:id',(request,response)=>{
-    const {id}=request.params
-    const idNumber=Number(id)
-    return response.json({
-        job:{id:idNumber,title:`Job with id ${id}`}
-    })
-})
-app.get('/a{b}cd',(request,response)=>{
+app.get('/job/:id',(req,res)=>{
+    const {id}=req.params
+    console.log(id)
     
-    return response.send('abcd o acd')
+   const job=jobs.find(job=>job.id===id)
+   if(!job){
+    return res.status(404).json({error:'Job not found'})
+   }
+   return res.json(job)
+
 })
-app.get('/bb*bb',(request,response)=>{
-    
-    return response.send('bb*bb')
+
+app.post('/jobs',(req,res)=>{
+    const {titulo,empresa,ubicacion,descripcion,data}=req.body 
+    const newJob={
+        id: crypto.randomUUID(),
+        titulo,
+        empresa,
+        ubicacion,
+        descripcion,
+        data
+    }
+    jobs.push(newJob)
+    return res.status(201).json(newJob)
+
 })
 
 app.listen(PORT,()=>{
---
+++ old_index.js
@@ -0,0 +1,63 @@
+import express, {request, response } from 'express'
+import process from 'node:process'
+import jobs from './jobs.json' with {type:'json'}
+import { join } from 'node:path'
+// Devuelve los segundos que el proceso lleva activo
+const uptime = process.uptime()
+
+const PORT=process.env.PORT ?? 1234
+
+const  app=express()
+app.use('/',(request,response,next)=>{
+    const timeString=new Date().toLocaleDateString()
+    console.log(`[${timeString} ${request.method} ${request.url}]`)
+    next()
+})
+app.use((req, res, next) => {
+  if (req.url.includes('com.chrome.devtools.json')) {
+    return next(); // Pasa sin imprimir nada
+  }
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.get('/',(request,response)=>{
+    return response.send('<h1>Hello Word!</h1>')
+})
+app.get('/health',(request,response)=>{
+    return response.json({
+        status:'ok',
+        uptime: Math.floor(process.uptime()), // Segundos ndeados
+        timestamp: Date.now(),
+    })
+})
+app.get('/get-jobs',async(request,response)=>{
+    // const {default:jobs}=await import('./jobs.json',{with:{type:'json'}})
+    const {text,title,limit,technology,offset}=request.query
+    if (text){
+        const searchTerm=text.toLocaleLowerCase()
+        filteredJobs=filteredJobs.filter(job=>{
+            job.titulo.toLocaleLowerCase().includes(searchTerm)||join
+        })
+    }
+    return response.json(jobs)
+})
+app.get('/get-single-job/:id',(request,response)=>{
+    const {id}=request.params
+    const idNumber=Number(id)
+    return response.json({
+        job:{id:idNumber,title:`Job with id ${id}`}
+    })
+})
+app.get('/a{b}cd',(request,response)=>{
+    
+    return response.send('abcd o acd')
+})
+app.get('/bb*bb',(request,response)=>{
+    
+    return response.send('bb*bb')
+})
+
+app.listen(PORT,()=>{
+    console.log(`Servidor levantado en http://localhost:${PORT}`)
+})
